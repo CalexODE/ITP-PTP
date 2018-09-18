c28b01eefc9da880ef0321baa386d58ef92ccbf1
CalexODE <33105149+CalexODE@users.noreply.github.com>
2018-09-18 15:39:48 -0300

    Add files via upload

---
+++ Prob2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+	/*decrarão da matriz e seus valores*/
+	int i=0,j=0;
+	do{
+		do{
+			printf("%d ", matriz[i][j]);
+			if (i == 0 || j == colunas)
+			{
+				cont++;
+				break;
+			}
+			i--;
+			j++;
+
+		}while(true);
+		i == cont;
+		j == 0
+	}while(i != linhas || j != colunas);
+	return 0;
+}
